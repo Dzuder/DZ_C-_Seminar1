509b98f72d42d88130bc2895881fa23f845483e5
Dzuder <dzuder14@yandex.ru>
2022-12-18 23:14:00 +0500

    create task 2

---
+++ Task002/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+WriteLine("Enter the first number: ");
+int a = int.Parse(ReadLine()!);
+WriteLine("Enter the second number: ");
+int b = int.Parse(ReadLine()!);
+WriteLine("Enter the third number: ");
+int c = int.Parse(ReadLine()!);
+int max = a;
+if (b > a)
+{
+max = b;
+}
+if (c > b)
+{
+max = c;
+}
+WriteLine("The largest number of three- " + max);
+
