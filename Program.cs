56bdc9e66b30751d964a2d790f50262055ce735a
Dzuder <dzuder14@yandex.ru>
2022-12-18 22:47:51 +0500

    create task 2

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+WriteLine("Enter the first number :");
+int a = int.Parse(ReadLine()!);
+WriteLine("Enter the second number :");
+int b = int.Parse(ReadLine()!);
+
+if (a > b)
+{
+    WriteLine("The largest number " + a);
+}
+else 
+{
+        WriteLine( "The largest number " + b);
+}
