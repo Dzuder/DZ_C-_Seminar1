2e7e6b0fd759eb1e41d6756718f2a7bddf847457
Dzuder <dzuder14@yandex.ru>
2022-12-18 23:33:37 +0500

    Create task 3

---
+++ Task003/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+WriteLine("Enter a number to find out if it is even");
+float a = float.Parse(ReadLine()!);
+float b = a % 2;
+if (b == 0)
+{
+    WriteLine("number " + a + ("- even"));
+}
+    if (b > 0)
+    {
+        WriteLine("number " + a + ("- odd"));
+    }
